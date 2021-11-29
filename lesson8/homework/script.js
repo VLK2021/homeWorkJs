2804b688a9b512e1d08e97f540edfea7adef653b
VLK2021 <volodymyrkostiuk2021@gmail.com>
2021-11-29 23:39:58 +0200

    create and finished and push  lesson8  homework homeWorkJs

---
+++ lesson8/homework/script.js
@@ -0,0 +1,60 @@
+// Візьміть файл template1.html, підключіть до нього скрипт, і працюйте в ньому!
+
+//     - Напишіть код,  який за допомоги document.getElementById або
+//     document.getElementsByClassName або document.getElementsByTagName :
+// -- отримує текст з параграфа з id "content"
+
+// let textIdContent = document.getElementById("content");
+// console.log(textIdContent.innerText);
+
+
+// -- отримує текст з блоку з id "rules"
+
+// let textIdRules = document.getElementById("rules");
+// console.log(textIdRules.innerText);
+
+
+// -- замініть текст параграфа з id 'content' на будь-який інший
+
+// let textIdContent = document.getElementById("content");
+// textIdContent.innerText = 'Міняю текст на який буде потрібно!!!!)))';
+
+
+// -- замініть текст параграфа з id 'rules' на будь-який інший
+
+
+// let textIdRules = document.getElementById("rules");
+// textIdRules.innerText = 'Міняю текст на який буде потрібно!!!!)))';
+
+
+// -- змініть кожному елементу колір фону на червоний
+
+// let children = document.body.children;
+// for (const element of children) {
+//     element.style.background = 'red'
+// }
+
+
+
+// -- змініть кожному елементу колір тексту на синій
+
+// let children = document.body.children;
+// for (const element of children) {
+//     element.style.color = 'blue';
+// }
+
+
+
+// -- отримати весь список класів елемента з id=rules і вивести їх в console.log
+
+// let idRules = document.getElementById('rules');
+// console.log(idRules.classList);
+
+
+// -- поміняти колір тексту у всіх елементів fc_rules на червоний
+
+//
+// let elementByClassName = document.getElementsByClassName('fc_rules');
+// for (const element of elementByClassName) {
+//     element.style.color = 'red';
+// }
