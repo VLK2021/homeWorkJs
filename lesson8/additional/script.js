e15af0c9fa325fd05e5ef847d6c31d42cde4e752
VLK2021 <volodymyrkostiuk2021@gmail.com>
2021-11-30 16:51:16 +0200

    create and finished and push  lesson8  additional homeWorkJs

---
+++ lesson8/additional/script.js
@@ -0,0 +1,31 @@
+// написати рекурсивну функцію, яка збирає всі назви класів з файлу rules.html в окремий масив.
+// масив вивести в консоль
+
+
+// let arr = [];
+// function func(elementBody) {
+//     if (elementBody.children.length){
+//         for (const element of elementBody.children) {
+//             if (element.classList.length){
+//                 for (const el of element.classList) {
+//                     arr.push(el);
+//                 }
+//             }
+//             func(element);
+//         }
+//     }
+// }
+// func(document.body);
+// console.log(arr);
+
+
+
+
+
+
+
+
+
+
+
+
