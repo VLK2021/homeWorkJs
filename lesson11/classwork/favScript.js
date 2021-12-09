fcc39c756b5bf9520978356f83afb47a7a28c839
VLK2021 <volodymyrkostiuk2021@gmail.com>
2021-12-09 22:50:41 +0200

    create and finished and push  lesson11  classwork homeWorkJs

---
+++ lesson11/classwork/favScript.js
@@ -0,0 +1,6 @@
+// let arr = JSON.parse(localStorage.getItem('keyObj'));
+// for (const arrElement of arr) {
+//     let div = document.createElement('div');
+//     div.innerText = JSON.stringify(arrElement);
+//     document.body.appendChild(div);
+// }
