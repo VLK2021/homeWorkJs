e595b92b243ef5f3ad90f803ca3bd05b6b81535e
VLK2021 <volodymyrkostiuk2021@gmail.com>
2021-12-19 22:17:12 +0200

    create and finished and push lesson14 homework only call-back hall  js

--- lesson14/homework/script.js
+++ lesson14/homework/script.js
@@ -162,5 +162,205 @@
 //     .catch(e => {
 //         console.error('error', e);
 //     })
+
+//============================================Cal-Back Hall==================================================================
+
+// function getAp(getAp, cb) {
+//     setTimeout(() => {
+//         if (getAp) {
+//             cb(null, 'Я просинаюсь!');
+//             return;
+//         }
+//         cb('I want sleep!');
+//     }, 900);
+// }
 //
+// function goBath(bath, cb) {
+//     setTimeout(() => {
+//         if (bath) {
+//             cb(null, 'іду у ванну!');
+//             return;
+//         }
+//         cb('I dont go to bathrom');
+//     }, 500);
+// }
+//
+// function goEat(eat, cb) {
+//     setTimeout(() => {
+//         if (eat) {
+//             cb(null, `Іду їсти ${eat}`);
+//             return;
+//         }
+//         cb('I do not go to kitchen(((!');
+//     }, 700);
+// }
+//
+// function goWork(gowork, cb) {
+//     setTimeout(() => {
+//         if (gowork) {
+//             cb(null, 'Я іду на роботу go go go');
+//             return;
+//         }
+//         cb('I stay at home');
+//     }, 100);
+// }
+//
+// function workOnTheWork(workwork, cb) {
+//     setTimeout(() => {
+//         if (workwork) {
+//             cb(null, 'Я працюю на роботі!');
+//             return;
+//         }
+//         cb('wrong!')
+//     }, 600);
+// }
+//
+// function goHome(gohome, cb) {
+//     setTimeout(() => {
+//         if (gohome) {
+//             cb(null, 'Я іду додому!!!!');
+//             return;
+//         }
+//         cb('I stay at work(((');
+//     }, 200);
+// }
+//
+// function watchTv(watch, cb) {
+//     setTimeout(() => {
+//         if (watch){
+//             cb(null, 'Я дивлюсь телевізор!');
+//             return;
+//         }
+//         cb('wrong');
+//     }, 400);
+// }
+//
+// function goSleep(sleep, cb) {
+//     setTimeout(() => {
+//         if (sleep){
+//             cb(null, 'Я іду спати!');
+//             return;
+//         }
+//         cb('wrong');
+//     }, 300);
+// }
+//
+//
+// getAp(true, (err, morning) => {
+//     if (err) {
+//         console.error('opppps!', err);
+//         return;
+//     } else {
+//         console.log(morning);
+//
+//         goBath(true, (err, bath) => {
+//             if (err) {
+//                 console.error('opppps!', err);
+//                 return;
+//             } else {
+//                 console.log(bath);
+//
+//                 goEat('омле та пити чай', (err, eat) => {
+//                     if (err) {
+//                         console.error('opppps!', err);
+//                         return;
+//                     } else {
+//                         console.log(`${eat}`);
+//
+//                         goWork(true, (err, gowork) => {
+//                             if (err) {
+//                                 console.error('opppps!', err);
+//                                 return;
+//                             } else {
+//                                 console.log('Я іду на роботу go go go');
+//
+//                                 workOnTheWork(true, (err, workwork) => {
+//                                     if (err) {
+//                                         console.error('opppps!', err);
+//                                         return;
+//                                     } else {
+//                                         console.log(workwork);
+//
+//                                         goEat( 'обід',(err, eat) => {
+//                                             if (err) {
+//                                                 console.error('opppps!', err);
+//                                                 return;
+//                                             } else {
+//                                                 console.log(eat);
+//
+//                                                 workOnTheWork(true, (err, workwork) => {
+//                                                     if (err) {
+//                                                         console.error('opppps!', err);
+//                                                         return;
+//                                                     } else {
+//                                                         console.log(workwork);
+//
+//                                                         goHome(true, (err, gohome) => {
+//                                                             if (err) {
+//                                                                 console.error('opppps!', err);
+//                                                                 return;
+//                                                             } else {
+//                                                                 console.log(gohome);
+//
+//                                                                 goBath(true, (err, bath) => {
+//                                                                     if (err) {
+//                                                                         console.error('opppps!', err);
+//                                                                         return;
+//                                                                     } else {
+//                                                                         console.log(bath);
+//
+//                                                                         goEat('вечерю', (err, eat) =>{
+//                                                                             if (err){
+//                                                                                 console.error('opppps!', err);
+//                                                                                 return;
+//                                                                             }else {
+//                                                                                 console.log(eat);
+//
+//                                                                                 watchTv(true, (err, watch) =>{
+//                                                                                     if (err){
+//                                                                                         console.error('opppps!', err);
+//                                                                                         return;
+//                                                                                     }else {
+//                                                                                         console.log(watch);
+//
+//                                                                                         goSleep(true, (err, sleep) =>{
+//                                                                                             if (err){
+//                                                                                                 console.error('opppps!', err);
+//                                                                                                 return;
+//                                                                                             }else {
+//                                                                                                 console.log(sleep);
+//                                                                                             }
+//                                                                                         })
+//
+//                                                                                     }
+//                                                                                 })
+//
+//                                                                             }
+//                                                                         })
+//
+//                                                                     }
+//                                                                 })
+//
+//                                                             }
+//                                                         })
+//                                                     }
+//                                                 })
+//
+//                                             }
+//                                         })
+//
+//                                     }
+//                                 })
+//
+//                             }
+//                         })
+//                     }
+//                 })
+//
+//             }
+//         })
+//
+//     }
+//
+// })
 //
