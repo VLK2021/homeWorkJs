abd01f8f8ed3ab41f894118d51c0217ed39f9656
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-08-29 18:13:56 +0300

    create and finished and push lesson14 homework only async await hall  js

---
+++ treining/script.js
@@ -0,0 +1,23 @@
+// class user {
+//     constructor(name, age, city) {
+//         this.name = name;
+//         this.age = age;
+//         this.city = city;
+//     }
+// }
+// let user1 = new user('vova', 38, 'lviv');
+// console.log(user1);
+
+// function user(name, age, city) {
+//     this.name = name;
+//     this.age = age;
+//     this.city = city;
+// }
+// let user1 = new user('vova', 38, 'lviv');
+// console.log(user1);
+
+
+//===================================================================================//
+
+
+
